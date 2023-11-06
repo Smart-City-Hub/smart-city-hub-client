fc675d63c8ad73e16824a6739cda008dd3c12b40
M Ibnu Umar R <fancycyanogen@gmail.com>
2023-11-06 20:01:37 +0700

    profile feature done

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { profileStore } from "../store/profile";
 
 function Avatar({ size, url }) {
   let width = "w-10";
@@ -6,13 +7,14 @@ function Avatar({ size, url }) {
     width = "w-24";
   }
   let avatarClass = `${width} rounded-full`;
+  const profile = profileStore(state => state.profile)
   return (
     <div>
       <div>
         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
           <div className={avatarClass}>
             <img
-              src="https://images.unsplash.com/photo-1593529467220-9d721ceb9a78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1930&q=80"
+              src={`http://localhost:3000/${profile.photo}`}
               alt="foto profil"
             />
           </div>
